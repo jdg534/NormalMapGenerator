6deae03df53602ed4b237f9c8a3135f8b95838cf
Josh <joshdgibson21@outlook.com>
2018-01-21 14:55:42 +0000

    Setup main project structure for dev with Qt5, next implerment and commit dummy UI

---
+++ MakeVS2013DevEnv.bat
@@ -0,0 +1,5 @@
+mkdir DEV_ENV
+cd DEV_ENV
+cmake .. -G "Visual Studio 12 2013"
+cd ..
+pause
---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is a place holder, next get a Qt window on screen." << std::endl;
+	
+	std::cin.get();
+	return 0;
+}
